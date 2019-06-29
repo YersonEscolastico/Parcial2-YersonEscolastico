e2ef815c2974a8ff69d9ef547f0d9f377bb09f0b
escolasticoy489@outlook.com <escolasticoy489@outlook.com>
2019-06-30 07:15:57 +0800

    Agregando entidad InscripcionesDetalle

---
+++ Parcial2-YersonEscolastico/Entidades/InscripcionesDetalle.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parcial2_YersonEscolastico.Entidades
+{
+    public class InscripcionesDetalle
+    {
+        [Key]
+        public int InscripcionDetallesId { get; set; }
+        public int InscripcionId { get; set; }
+        public int AsignaturaId { get; set; }
+        public decimal SubTotal { get; set; }
+
+        [ForeignKey("AsignaturaId")]
+        public virtual Asignaturas Asignatura { get; set; }
+
+        public InscripcionesDetalle()
+        {
+            InscripcionDetallesId = 0;
+            InscripcionId = 0;
+            AsignaturaId = 0;
+            SubTotal = 0;
+        }
+
+        public InscripcionesDetalle(int id, int inscripcionId, int asignaturaId, decimal subtotal)
+        {
+            InscripcionDetallesId = id;
+            InscripcionId = inscripcionId;
+            AsignaturaId = asignaturaId;
+            SubTotal = subtotal;
+        }
+    }
+}
