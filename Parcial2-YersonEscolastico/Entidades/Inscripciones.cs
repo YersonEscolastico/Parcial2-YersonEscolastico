aa1ab7bcf31bb6f8f92f43bd0a268a38667a3113
escolasticoy489@outlook.com <escolasticoy489@outlook.com>
2019-06-30 07:07:49 +0800

    Agregando entidad Inscripciones

---
+++ Parcial2-YersonEscolastico/Entidades/Inscripciones.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parcial2_YersonEscolastico.Entidades
+{
+    public class Inscripciones
+    {
+        [Key]
+        public int InscripcionId { get; set; }
+        public int EstudianteId { get; set; }
+        public DateTime Fecha { get; set; }
+        public decimal Monto { get; set; }
+
+        public Inscripciones()
+        {
+            InscripcionId = 0;
+            EstudianteId = 0;
+            Fecha = DateTime.Now;
+            Monto = 0;
+        }
+    }
+}
